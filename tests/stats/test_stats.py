bf731c71f16692daa709b04d79f6d0ab004de7ef
javierjsa <javierjsa@users.noreply.github.com>
2023-05-21 12:32:42 +0200

    Added tests, docstrings, typing, minor changes
    - Added tests for StatsManager data loading and date validation
    - Added docstrings and typing to StatsManager
    - Improved test endpoints docstrings and typing
    - Capture ValueError exception caused by malformed date strings

--- tests/stats/test_stats.py
+++ tests/stats/test_stats.py
@@ -1,15 +1,58 @@
+import datetime
 import unittest
+from unittest.mock import Mock,patch
 from statsapi.stats.stats_manager import StatsManager
-
-
+from statsapi.stats.utils import StatsManagerException
 
 class TestStats(unittest.TestCase):
 
     @classmethod
-    def setUp(cls) -> None:
+    def setUpClass(cls):
 
         cls.manager = StatsManager()
 
-    def test_mean(self):
+    def test_data_loading(self) -> None:
+        """
+        Assert data is not loaded once a previous StatsManager instance exists
+        :return: None
+        """
 
-        manager.co
+        with patch.object(StatsManager, 'load_data') as mock:
+            mock.side_effect = Exception("Attempted to load data")
+            _ = StatsManager()
+
+    def test_validate_dates(self) -> None:
+        """
+        Check valid combinations of start and end dates
+        :return: None
+        """
+
+        start_date, end_date = self.manager.validate_dates()
+        self.assertIsNone(start_date)
+        self.assertIsNone(end_date)
+
+        start_date, end_date = self.manager.validate_dates(start_date="2019-05-01")
+        self.assertEqual(type(start_date), datetime.datetime)
+        self.assertEqual(type(end_date), datetime.datetime)
+        now = datetime.datetime.now()
+        self.assertEqual(now.date(), end_date.date())
+        self.assertTrue(start_date < end_date)
+
+        start_date, end_date = self.manager.validate_dates(start_date="2019-05-01", end_date="2019-07-01")
+        self.assertEqual(type(start_date), datetime.datetime)
+        self.assertEqual(type(end_date), datetime.datetime)
+        self.assertTrue(start_date < end_date)
+
+    def test_fail_date_validation(self) -> None:
+        """
+        Assert custom exception is raised whenever a malformed date range is provided
+        :return: None
+        """
+        with self.assertRaises(StatsManagerException):
+            _, _ = self.manager.validate_dates(end_date="2019-05-01")
+        with self.assertRaises(StatsManagerException):
+            _, _ = self.manager.validate_dates(start_date="2019-07-01", end_date="2019-05-01")
+        with self.assertRaises(StatsManagerException):
+            _, _ = self.manager.validate_dates(start_date="20-07-01", end_date="20-05-01")
+
+
